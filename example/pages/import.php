ea39589d9f7b46f278d6b11655a6d74d54cf6b91
mars.hung <tfaredxj@gmail.com>
2018-08-15 21:57:03 +0800

    [Patch] example: optimize import output format

--- example/pages/import.php
+++ example/pages/import.php
@@ -10,6 +10,7 @@ $data = $io->import($builder = 'Excel', $fileArgu = 'fileupload');
 
 $configName = $io->getConfig()->getOption('configName');
 
+echo '<pre>';
 echo 'Config Name = ' . $configName . "<br>\n";
 echo 'Data = ';
 var_export($data);
